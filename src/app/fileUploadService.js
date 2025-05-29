7d268f3650ddbc20c4e6394c9b9b6575a3d41221
hellofadude <geekinthelead@gmail.com>
2025-05-29 08:56:09 +0100

    added navigaton, remove debug statements

--- src/app/fileUploadService.js
+++ src/app/fileUploadService.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import handleUpload from "./handleUpload"; 
 import { useActionState } from "react";
+import Link from 'next/link'
 
 
 
@@ -11,26 +12,31 @@ export default function Fileupload() {
    
   return(
     <>
-     <form action={formAction}>
-          <div style={{display:'block'}}>
-
+        <form action={formAction}>
+          <div>
+            <div>
                <label>
                   File to parse:  <input
                       type='file'
                       name='myFile' />
                </label>
-                        
-               <button type="submit">upload</button>       
+            </div>
+            <div>                      
+               <button type="submit">upload</button> 
+            </div>      
                {isPending ? "Loading..." : 
-                 state === "" ? "" : 
-                 <div style={{ border: '1px solid black', padding: '10px', marginTop: '100px' }}>
-                    <h3>Report</h3>
-                       <pre style={{ whiteSpace: 'pre-wrap' }}>{state}</pre>
-                 </div> 
+                 state === "" ? "" :
+                 <div>
+                    <div style={{ border: '1px solid black', padding: '10px', marginTop: '100px' }}>
+                       <h3>Report</h3>
+                       <pre style={{ whiteSpace: 'pre-wrap' }}>{state}</pre>   
+                    </div>
+                    <p>Refresh to go back</p>
+                  </div>
                }
-         
+                     
           </div>        
-      </form>
+        </form>
   
     </>
   )  
